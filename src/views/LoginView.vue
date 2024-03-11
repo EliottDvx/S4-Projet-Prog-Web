0444633c0d9f8ad5ab360a6da6c5d5aa9c63e71c
EliottDvx <eliottdevaux@gmail.com>
2024-03-11 14:28:53 +0100

    route auto if token exists or not

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,18 @@
 <script setup>
     import ConnectSpotifyBtn from '@/components/ConnectSpotifyBtn.vue';
+
+    import { useRouter, useRoute } from 'vue-router'
+    import { onMounted } from 'vue'
+    import { cookieValueOrNull } from '@/utils/cookieCheck'
+
+    const router = useRouter()
+    const route = useRoute()
+
+    onMounted(() => {
+        cookieValueOrNull('accessToken')
+        ? router.replace('/') : {}
+    })
+
 </script>
 
 <template>
