54793ce1b24a7005a5046837557544e2c15bf082
EliottDvx <eliottdevaux@gmail.com>
2024-03-26 15:28:56 +0100

    style select

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,12 +11,48 @@ const selectedTimeRange = ref('medium_term');
 <template>
   <main>
     <h1>Your Profile Main Board</h1>
-    <select v-model="selectedTimeRange">
-      <option value="short_term">Short term (4 weeks)</option>
-      <option value="medium_term" selected="selected">Medium term (6 months)</option>
-      <option value="long_term">Long term (Several years)</option>
-    </select>
+    <p>Here you can see your favorite tracks and artists from different time ranges.</p>
+    <div class="selectContainer">
+      <select v-model="selectedTimeRange" size="3">
+        <option value="short_term">Short term (4 weeks)</option>
+        <option value="medium_term" selected="selected">Medium term (6 months)</option>
+        <option value="long_term">Long term (Several years)</option>
+      </select>
+    </div>
     <FavTracks :range="selectedTimeRange" />
     <FavArtists :range="selectedTimeRange" />
   </main>
 </template>
+
+<style scoped>
+.selectContainer {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+select {
+  appearance: none;
+  overflow-y: auto;
+  text-align: center;
+  background-color: white;
+  border: none;
+  border-radius: 5px;
+  width: 100%;
+  outline: none
+}
+
+select option {
+  padding: 1em;
+}
+
+select option:checked {
+  background-color: #aaa;
+}
+
+@media (min-width: 1024px) {
+  select {
+    width: 30%;
+  }
+}
+</style>
