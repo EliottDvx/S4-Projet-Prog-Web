01653900176a5e13213a1840a316d358ccaf525a
EliottDvx <eliottdevaux@gmail.com>
2024-04-15 19:17:18 +0200

    rm play hover

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -12,13 +12,6 @@ const dateYear = new Date(props.date).getFullYear();
 
 <template>
     <li>
-        <div class="play-overlay">
-            <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white">
-                    <path d="M3 22v-20l18 10-18 10z" />
-                </svg>
-            </button>
-        </div>
         <img :src="image" alt="Album cover" />
         <div class="infos">
             <h3>{{ title }}</h3>
@@ -48,30 +41,6 @@ img {
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
-}
-
-.play-overlay {
-    position: absolute;
-    width: 100px;
-    height: 100px;
-    background-color: rgba(0, 0, 0, 0.5);
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    opacity: 0;
-    transition: opacity 0.3s;
-}
-
-.play-overlay button {
-    background-color: transparent;
-    border: none;
-    transition: transform 0.3s;
-}
-
-li:hover .play-overlay {
-    cursor: pointer;
-    opacity: 1;
 }
 
 @media (min-width: 1024px) {
